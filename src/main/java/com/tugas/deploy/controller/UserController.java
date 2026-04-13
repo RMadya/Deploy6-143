d0c146084432e37ae4304a1bffddabdb3f8aa66f
RMadya <kereyoben75@gmail.com>
2026-04-13 15:07:45 +0700

    add  usercontroller

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.tugas.deploy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping({"/", "/login", "/login.html"})
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping({"/home", "/home.html"})
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping({"/form", "/form.html"})
+    public String formPage() {
+        return "form";
+    }
 }
